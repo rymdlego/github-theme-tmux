9e8cbc32ce9d9c36fae7126d72959582706c7f62
Jakob <jakob@sub.se>
2024-10-21 15:51:04 +0200

    fix: azure short name

--- scripts/azure_subscription.sh
+++ scripts/azure_subscription.sh
@@ -5,10 +5,13 @@ azure_subscription() {
   local subscription
   subscription="$(jq -r '.subscriptions[] | select(.isDefault == true) | .name' ~/.azure/azureProfile.json 2>/dev/null)"
 
+  local shortname
+  shortname="$(echo $subscription | sed -n 's/.*(\(.*\)).*/\1/p')"
+
   # Check if the subscription exists
   if [ -n "$subscription" ]; then
     # Return the decorated subscription if it exists
-    echo " #[fg=$2,bg=$1]#[fg=$3,bg=$2] 󰠅 $subscription #[fg=$2,bg=$1]"
+    echo " #[fg=$2,bg=$1]#[fg=$3,bg=$2] 󰠅 $shortname #[fg=$2,bg=$1]"
   else
     # Return nothing if no subscription is set
     echo ""
